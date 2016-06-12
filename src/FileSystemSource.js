3a830f1624f0199254fad3667d5ddcbb88d1c1fc
Gerrard Lindsay <gerrardalindsay@gmail.com>
2016-06-12 16:59:28 -0400

    implement fs source.

--- src/FileSystemSource.js
+++ src/FileSystemSource.js
@@ -2,6 +2,31 @@
 
 const events = require('events');
 const fs = require('fs');
+const path = require('path');
+
+function walkDirectory(fsSource, targetPath, rootPath) {
+  const entries = [];
+  let isRoot = !rootPath;
+
+  if (isRoot) {
+    rootPath = targetPath;
+  }
+
+  const files = fs.readdirSync(targetPath);
+   
+  for (let file of files) {
+    const fullPath = path.join(targetPath, file);
+    entries.push(fullPath);
+
+    if(fs.statSync(fullPath).isDirectory()) {
+        const childEntries = walkDirectory(fsSource, fullPath, rootPath); 
+        //TODO: we really need to process the full chain, could have git children
+        entries.push(...childEntries);
+    }
+  }
+
+  return entries;
+}
 
 class FileSystemSource extends events.EventEmitter
 {
@@ -9,27 +34,14 @@ class FileSystemSource extends events.EventEmitter
     super();
   }
 
-  readFiles(path) {
-    fs.readdir(path, (err, data) => {
-      // TODO: error handling
-    });
-  }
+  readFiles(targetPath) {
+    const entries = walkDirectory(this, targetPath);
 
-  _handleDataCallbacks(data) {
-    this.emit('data', data);
-  }
-
-  _handleDoneCallbacks() {
-    this.emit('done');
-  }
-
-  _handleErrorCallbacks(error) {
-    // if we're just not in a git repo, just emit done
-    if (error.match(/Not a git repository/)) {
-      this.emit('notsupported');
-    } else {
-      this.emit('error', error);
+    for(let entry of entries) {
+      this.emit('data', path.relative(targetPath, entry));
     }
+
+    this.emit('done');
   }
 }
 
